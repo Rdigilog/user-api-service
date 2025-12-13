309455a6b8eda4b538f4c10bafde79141d6f6fe8
UmarRabi <tipson664@gmail.com>
2025-12-13 11:17:50 +0100

    added firbse social login

--- src/utils/providers/firebase.auth.provider.ts
+++ src/utils/providers/firebase.auth.provider.ts
@@ -1,18 +1,22 @@
 import * as admin from 'firebase-admin';
+import { ConfigService } from '@nestjs/config';
 
 export const firebaseAdminProvider = {
   provide: 'FIREBASE_ADMIN',
-  useFactory: () => {
-    if (admin.apps.length === 0) {
+  useFactory: (configService: ConfigService) => {
+    if (!admin.apps.length) {
       admin.initializeApp({
         credential: admin.credential.cert({
-          projectId: process.env.FIREBASE_PROJECT_ID,
-          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-          privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+          projectId: configService.get<string>('FIREBASE_PROJECT_ID'),
+          clientEmail: configService.get<string>('FIREBASE_CLIENT_EMAIL'),
+          privateKey: configService
+            .get<string>('FIREBASE_PRIVATE_KEY')
+            ?.replace(/\\n/g, '\n'),
         }),
       });
     }
 
     return admin;
   },
+  inject: [ConfigService],
 };
